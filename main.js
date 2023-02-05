aaa8109c5179628c78ffde517808474b4b09beec
martinw-dev <martinwalsh.dev@gmail.com>
2023-02-05 10:39:15 +0000

    text-to-image UI

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector("form");
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  showSpinner();
+
+  const data = new FormData(form);
+
+  const response = await fetch("http://localhost:8080/api/photo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+  if (response.ok) {
+    const { image } = await response.json();
+
+    const result = document.querySelector("#result");
+    result.innerHTML = `<img src="${image}" width="512" />`;
+  } else {
+    const error = await response.text();
+    alert(error);
+    console.log(error);
+  }
+  hideSpinner();
+});
+
+function showSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = true;
+  button.innerHTML = 'Generating... <span class="spinner">🤖</span>';
+}
+
+function hideSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = false;
+  button.innerHTML = "Generate";
+}
+
+// write a readme for this project
+
+
